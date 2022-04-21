520b62218ad93be320ebfadd6fce861f4b2d0778
Willian Orihuela <worihuela@unsa.edu.pe>
2022-04-20 23:55:44 -0500

    segundo commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { BannerComponent } from './banner/banner.component';
+import { ProductosComponent } from './productos/productos.component';
 
 
 
@@ -12,7 +13,8 @@ import { BannerComponent } from './banner/banner.component';
     NavbarComponent,
     LandingComponent,
     CatalogoComponent,
-    BannerComponent
+    BannerComponent,
+    ProductosComponent
   ],
   exports: [
     NavbarComponent,
